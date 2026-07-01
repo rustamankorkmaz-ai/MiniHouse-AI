ee579e807e64733e1babd0697205ada3f40ce5ff
rustamankorkmaz-ai <rustamankorkmaz@gmail.com>
2026-07-01 16:39:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Я MiniHouse-AI 🤖")
 
-TOKEN = "ВСТАВИМ_ПОЗЖЕ"
+TOKEN = "os.getenv "
 
 app = Application.builder().token(TOKEN).build()
 
