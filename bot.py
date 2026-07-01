4ee369639b6565e48fa6b490b4fdb503d573d4df
rustamankorkmaz-ai <rustamankorkmaz@gmail.com>
2026-07-01 16:34:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,16 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
-print("MiniHouse-AI запущен!")
-print("Бот готов к работе")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привет! Я MiniHouse-AI 🤖")
+
+TOKEN = "ВСТАВИМ_ПОЗЖЕ"
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+print("Бот запущен!")
+
+app.run_polling()
+
